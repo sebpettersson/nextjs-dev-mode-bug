ae6860b06a0f44c6143dc77f9c566cf6590df1e3
Sebastian Pettersson <seb.pettersson@gmail.com>
2023-09-13 14:08:15 +0200

    Update Next.js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: "/dev-mode-bug",
+  rewrites: async () => [
+    {
+      source: `/dev-mode-bug/_next/static/:path*`,
+      destination: "/_next/static/:path*",
+    },
+  ],
+};
